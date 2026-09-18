b35d88bc70b01c2ab0f8795370a11dba75450432
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-09-18 22:19:41 +0100

    Create xml2rfcv3.mk

---
+++ xml2rfcv3.mk
@@ -0,0 +1,69 @@
+# XSLT processor of choice
+XSLT=saxon -now:$(shell date -r $< -u +%Y-%m-%dT%H:%M:%SZ)
+
+all: \
+	draft-iab-rfc7991bis.redxml \
+	draft-iab-rfc7991bis.txt \
+	xml2rfcv3-annotated.rng
+#	draft-iab-rfc7991bis.unpg.txt \
+
+allhtml: \
+	draft-iab-rfc7991bis.redxml \
+	xml2rfcv3-annotated.rng \
+	draft-iab-rfc7991bis.html
+
+xml2rfc.all: \
+	draft-iab-rfc7991bis.xml xml2rfcv3-annotated.rng
+
+xml2rfcv3.rnc: xml2rfcv3.rng SVG-1.2-RFC.rnc SVG-1.2-RFC.rng
+	java -jar trang.jar -o lineLength=69 $< $@
+
+#xml2rfcv3.dtd: xml2rfcv3.rng
+#	java -jar trang.jar $< $@
+
+xml2rfcv3-annotated.rng: xml2rfcv3.rng annotate-rng.xslt draft-iab-rfc7991bis.xml
+	$(XSLT) $< annotate-rng.xslt doc=draft-iab-rfc7991bis.xml > $@
+
+xml2rfcv3-spec.xml: xml2rfcv3.rng rng2xml2rfc.xslt
+	$(XSLT) $< rng2xml2rfc.xslt voc=v3 specsrc=draft-iab-rfc7991bis.xml > $@
+
+xml2rfcv3-spec-deprecated.xml: xml2rfcv3.rng rng2xml2rfc.xslt
+	$(XSLT) $< rng2xml2rfc.xslt voc=v3 specsrc=draft-iab-rfc7991bis.xml deprecated=yes > $@
+#	$(XSLT) $< rng2xml2rfc.xslt specsrc=draft-iab-rfc7991bis.xml deprecated=yes > $@
+
+xml2rfcv3.rnc.folded: xml2rfcv3.rnc
+	./fold-rnc.sh $< | tr -d "\\015" > $@
+
+draft-iab-rfc7991bis.xml: xml2rfcv3-spec.xml xml2rfcv3-spec-deprecated.xml xml2rfcv3.rnc.folded differences-from-v2.txt
+	cp -v $@ $@.bak
+	./refresh-inclusions.sh $@
+
+xml2rfcv2 = xml2rfcv2.rnc
+
+differences-from-v2.txt:	xml2rfcv3.rnc $(xml2rfcv2)
+	fold -w66 -s $(xml2rfcv2) > $@.v2
+	fold -w66 -s $<  > $@.v3
+	diff -w --old-line-format='- %L' --new-line-format='+ %L' \
+	--unchanged-line-format='  %L' -d $@.v2 $@.v3 \
+	| sed "s/\&/\&amp;/g" | tr -d "\\015" > $@
+	rm -f $@.v2 $@.v3
+
+%.redxml:	%.xml
+	$(XSLT) -l $< clean-for-xml2rfc-v3.xslt > $@
+
+%.txt:	%.redxml
+	xml2rfc --v3 --text $< -o $@
+
+%.html:	%.redxml
+	xml2rfc --v3 --html $< -o $@
+
+%.unpg.txt:	%.redxml
+	xml2rfc --v3  --no-pagination $< -o $@
+
+SVG-1.2-RFC.rnc:
+	wget https://raw.githubusercontent.com/ietf-tools/xml2rfc/main/xml2rfc/data/SVG-1.2-RFC.rnc
+#	wget https://svn.tools.ietf.org/svn/tools/xml2rfc/trunk/cli/xml2rfc/data/SVG-1.2-RFC.rnc
+
+SVG-1.2-RFC.rng:
+	wget https://raw.githubusercontent.com/ietf-tools/xml2rfc/main/xml2rfc/data/SVG-1.2-RFC.rng
+#	wget https://svn.tools.ietf.org/svn/tools/xml2rfc/trunk/cli/xml2rfc/data/SVG-1.2-RFC.rng
